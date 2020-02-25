b76b6ca822128cecad2d08d01f6308cbb0f718a9
BSS <eman.shedeed@black-squares.com>
2020-02-25 17:22:46 +0200

    First commit

---
+++ Pods/Target Support Files/Pods-PrayerMate/Pods-PrayerMate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PrayerMate : NSObject
+@end
+@implementation PodsDummy_Pods_PrayerMate
+@end
--- PrayerMate/ViewController.swift
+++ PrayerMate/ViewController.swift
@@ -7,14 +7,111 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import GoogleAPIClientForREST
+import GoogleSignIn
+import GTMSessionFetcher
+class ViewController: UIViewController{
+    private let scopes = [kGTLRAuthScopeCalendar]
+    private let service = GTLRCalendarService()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Configure Google Sign-in.
+        GIDSignIn.sharedInstance().clientID = "869556618508-cfup3e2uapcih6kcanfeacq7rl0hpri9.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().scopes = scopes
+        GIDSignIn.sharedInstance()?.presentingViewController = self
     }
+    @IBAction func googleSignInBtnPressed(_ sender: UIButton) {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    
+    // Create an event to the Google Calendar's user
+    func addEventoToGoogleCalendar(summary : String, description :String, startTime : String, endTime : String) {
+        let calendarEvent = GTLRCalendar_Event()
+        
+        calendarEvent.summary = "\(summary)"
+        calendarEvent.descriptionProperty = "\(description)"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+        let startDate = dateFormatter.date(from: startTime)
+        let endDate = dateFormatter.date(from: endTime)
+        
+        guard let toBuildDateStart = startDate else {
+            print("Error getting start date")
+            return
+        }
+        guard let toBuildDateEnd = endDate else {
+            print("Error getting end date")
+            return
+        }
+        calendarEvent.start = buildDate(date: toBuildDateStart)
+        calendarEvent.end = buildDate(date: toBuildDateEnd)
+        
+        let insertQuery = GTLRCalendarQuery_EventsInsert.query(withObject: calendarEvent, calendarId: "primary")
+        
+        service.executeQuery(insertQuery) { (ticket, object, error) in
+            if error == nil {
+                print("Event inserted")
+            } else {
+                print(error)
+            }
+        }
+    }
+    
+    // Helper to build date
+    func buildDate(date: Date) -> GTLRCalendar_EventDateTime {
+        let datetime = GTLRDateTime(date: date)
+        let dateObject = GTLRCalendar_EventDateTime()
+        dateObject.dateTime = datetime
+        return dateObject
+    }
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertController.Style.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertAction.Style.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+}
 
 
+extension ViewController:GIDSignInDelegate{
+    //MARK:Google SignIn Delegate
+    func sign(inWillDispatch signIn: GIDSignIn!, error: Error!) {
+        // myActivityIndicator.stopAnimating()
+    }
+    // Present a view that prompts the user to sign in with Google
+    func sign(_ signIn: GIDSignIn!,
+              present viewController: UIViewController!) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    // Dismiss the "Sign in with Google" view
+    func sign(_ signIn: GIDSignIn!,
+              dismiss viewController: UIViewController!) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    ////Google_signIn
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            self.service.authorizer = nil
+        } else {
+            self.service.authorizer = user.authentication.fetcherAuthorizer()
+            addEventoToGoogleCalendar(summary: "summary9", description: "description", startTime: "25/02/2020 09:00", endTime: "25/02/2020 10:00")
+        }
+    }
 }
 
