e1acb3caace95eadae274586766aa132a16a657e
BSS <eman.shedeed@black-squares.com>
2020-02-25 08:18:20 +0200

    Initial Commit

---
+++ PrayerMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrayerMate
+//
+//  Created by eman shedeed on 2/25/20.
+//  Copyright © 2020 eman shedeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
